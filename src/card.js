b03c80b2078b9dee6cd0854bbc782bc7fa4f7ae6
JurRodi <rodijurrien@gmail.com>
2022-09-30 15:12:23 +0200

    Feat: add check and save on click

--- src/card.js
+++ src/card.js
@@ -1,3 +1,5 @@
+import Bingo from "../main";
+
 export default class Card {
   constructor(title) {
     // the constructor is called when you create a new instance of the class
@@ -29,9 +31,11 @@ export default class Card {
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
     card.addEventListener("click", (e) => {
-      // this.markDone(e.target);
+      this.markDone(e.target);
       // call checkWinner() on the Bingo class
       // try to call the save() method on the Bingo class
+      Bingo.checkWinner();
+      Bingo.save();
     });
   }
 }
